664d027d1cd6e4d6f2760c9324a35491f69a79ca
jakegaia <jake@gaiaresources.com.au>
2016-10-18 08:56:41 +0800

    Update geoip.js

--- geoip.js
+++ geoip.js
@@ -1,27 +1,48 @@
-// Call GeoIP URL from user's location
-var url = "http://freegeoip.net/json/";
+var long = 10
+var lat = 20
 
-// Grab latitude and longitude from user's ip location
-var point = url["latitude"] + url["longitude"]; 
+// Call GeoIP URL from user's location
+$.get("http://freegeoip.net/json", function(response) {
+    //if your site is using SSL then use https instead of http
+    long = response.longitude;
+    lat = response.latitude;
+    console.log(long, lat);
+}, "jsonp");
+
+// Another GeoIP service
+// $.get("http://ipinfo.io", function(response) {
+//     var point = response.loc;
+//     console.log(response.loc);
+// }, "jsonp");
+
+// Create point json from geoip long/lat
+var point = {
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "Point",
+    "coordinates": [long, lat]
+  }
+};
+
+// var myregions = JSON.parse(regions);
 
 // Loop through qld region dataset to find where latitude/longitude are within
 for(var i = 0; i < regions.features.length; i++) {
     regions.features[i].properties = {};
-    regions.features[i].bbox = turf.extent(regions.features[i]);
+    regions.features[i].bbox = turf.bbox(regions.features[i]);
     regions.features[i].properties.point = 0;
     for(var j = 0; j < point.features.length; j++) {
         if(point.features[j].geometry.coordinates[0] >= regions.features[i].bbox[0] &&
-           point.features[j].geometry.coordinates[0] <= regions.features[i].bbox[2] &&
-           point.features[j].geometry.coordinates[1] >= regions.features[i].bbox[1] &&
-           point.features[j].geometry.coordinates[1] <= regions.features[i].bbox[3] &&
-           turf.inside(point.features[j],regions.features[i])) {
+            point.features[j].geometry.coordinates[0] <= regions.features[i].bbox[2] &&
+            point.features[j].geometry.coordinates[1] >= regions.features[i].bbox[1] &&
+            point.features[j].geometry.coordinates[1] <= regions.features[i].bbox[3] &&
+            turf.inside(point.features[j],regions.features[i])) {
             // Don't think this part is needed ??
             // regions.features[i].properties.point++;
+            result = "Queensland";
+        } else {
+            result = regions[0].ID;
         }
     }
 }
-
-
-
-// var regions = 'QLD_LGA_POLYGON_shp_diss_smooth5.geojson'
-// point.properties.title = turf.inside(point, regions);
